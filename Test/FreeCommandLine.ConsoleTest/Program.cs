d2ffff77ae311aaf951f91579b1711428bba18f4
王学懿 <xueyi.wang@macrowing.com>
2023-09-11 16:02:52 +0800

    添加测试项目

---
+++ Test/FreeCommandLine.ConsoleTest/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace FreeCommandLine.ConsoleTest
+{
+    internal class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            string warning = "warn";
+            string error = "error";
+
+            int paddingAmount = 10;
+
+            ShowInfo(warning, paddingAmount);
+            ShowInfo(error, paddingAmount);
+
+            Console.ReadLine();
+
+            //var table = new ConsoleTable("one", "two", "three");
+            //table.AddRow(1, 2, 3)
+            //     .AddRow("this line should be longer", "yes it is", "oh");
+            //// 打印
+            //table.Write();
+        }
+
+        public static void ShowInfo(string str, int lenght)
+        {
+            Console.WriteLine($"[{str.PadLeft(lenght)}]");
+        }
+    }
+}
---
+++ Test/FreeCommandLine.ConsoleTest/TestOption.cs
@@ -0,0 +1,38 @@
+﻿using FreeCommandLine.Attr;
+using FreeCommandLine.Model;
+
+namespace FreeCommandLine.ConsoleTest
+{
+    [GlobalOpt]
+    [HelpText("这是一个测试命令")]
+    public class TestGlobalOpt
+    {
+        [Opt("h", "host")]
+        public string Host { get; set; }
+    }
+
+    [Command("file", "f")]
+    [HelpText("这是一个测试命令")]
+    public class TestCommand
+    {
+        [Opt("num", "n")]
+        [HelpText("这是一个测试命令")]
+        [Usage("测试时", Example = "10")]
+        public int? Num { get; set; }
+
+        /*
+         *
+         * [用法]
+         * 测试时:
+         * file -n 10
+         */
+
+        [Value(index: 0)]
+        public string? Url { get; set; }
+
+        public void ShowTestInfo(GlobalOpt<TestGlobalOpt> gopt)
+        {
+            Console.WriteLine($"调用了test命令,获取到的选项是:[{this.Num}],获取到的Url值为:[{this.Url}]");
+        }
+    }
+}
